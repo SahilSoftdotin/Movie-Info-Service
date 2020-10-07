650fffe46b28f9c6137036ccc65bd59f6427c728
Sahil Arora <sahilsoft.arora@gmail.com>
2020-10-07 12:00:40 +0530

    Movie-info-service is calling external moviedb.org API to get movie data

--- movie-info-service/src/main/java/io/sahilsoft/movieinfoservice/models/Movie.java
+++ movie-info-service/src/main/java/io/sahilsoft/movieinfoservice/models/Movie.java
@@ -3,33 +3,38 @@ package io.sahilsoft.movieinfoservice.models;
 public class Movie {
 	
 	private String movieId;
-	private String name;
-	
-	public Movie() {
-	}
-	
-	public Movie(String movieId, String name) {
-		super();
-		this.movieId = movieId;
-		this.name = name;
-	}
-	@Override
-	public String toString() {
-		return "Movie [movieId=" + movieId + ", name=" + name + "]";
-	}
-	public String getMovieId() {
-		return movieId;
-	}
-	public void setMovieId(String movieId) {
-		this.movieId = movieId;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	
+    private String name;
+    private String description;
+
+    public Movie(String movieId, String name, String description) {
+        this.movieId = movieId;
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(String movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 	
 
 }
